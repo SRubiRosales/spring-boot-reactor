bcbad6a04133ab3a740fe7b80c851b2da6c93057
SRubiRosales <sharon_rosales98@hotmail.com>
2023-09-01 18:05:31 -0600

    Metodo suscribe y excepcion

--- src/main/java/com/srosales/springboot/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/srosales/springboot/reactor/app/SpringBootReactorApplication.java
@@ -1,5 +1,7 @@
 package com.srosales.springboot.reactor.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,14 +10,21 @@ import reactor.core.publisher.Flux;
 @SpringBootApplication
 public class SpringBootReactorApplication implements CommandLineRunner {
 
+	private static final Logger log = LoggerFactory.getLogger(SpringBootReactorApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootReactorApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		Flux<String> nombres = Flux.just("Sharon", "Ruby", "Andrea")
-				.doOnNext(System.out::println);
-		nombres.subscribe();
+		Flux<String> nombres = Flux.just("Sharon", "Ruby", "", "Andrea")
+				.doOnNext(e -> {
+					if (e.isEmpty()) {
+						throw new RuntimeException("Nombres no pueden ser vacios");
+					}
+					System.out.println(e);
+				});
+		nombres.subscribe(log::info,
+				error -> log.error(error.getMessage()));
 	}
 }
