5e68e865d5864158a420572e5b0fbff881f0fbfa
SRubiRosales <sharon_rosales98@hotmail.com>
2023-09-15 16:13:56 -0600

    Operador map con modelo Usuario

--- src/main/java/com/srosales/springboot/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/srosales/springboot/reactor/app/SpringBootReactorApplication.java
@@ -1,5 +1,6 @@
 package com.srosales.springboot.reactor.app;
 
+import com.srosales.springboot.reactor.app.models.Usuario;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -17,14 +18,20 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Flux<String> nombres = Flux.just("Sharon", "Ruby", "Maria", "Andrea")
-				.doOnNext(e -> {
-					if (e.isEmpty()) {
+		Flux<Usuario> nombres = Flux.just("Sharon", "Ruby", "Maria", "Andrea")
+				.map(nombre -> new Usuario(nombre.toUpperCase(), null))// Method reference
+				.doOnNext(usuario -> {
+					if (usuario == null) {
 						throw new RuntimeException("Nombres no pueden ser vacios");
 					}
-					System.out.println(e);
+					System.out.println(usuario.getNombre());
+				})
+				.map(usuario -> {
+					String nombre = usuario.getNombre().toLowerCase();
+					usuario.setNombre(nombre);
+					return usuario;// Lambda
 				});
-		nombres.subscribe(log::info,
+		nombres.subscribe(e -> log.info(e.toString()),
 				error -> log.error(error.getMessage()),
 					new Runnable() {
 						@Override
